213b17f041d6bae898c2e7a17a9b33186e9b6a8f
Klainti Bontourri <kbontourri@gmail.com>
2017-03-14 10:56:21 -0700

    Add files for hw1

---
+++ find_roots.c
@@ -0,0 +1,18 @@
+                                                                                 
+#include <linux/sched.h>
+                                                         
+SYSCALL_DEFINE0(find_roots){                                                     
+                                                                                  
+   struct task_struct *tmp;                                                     
+                                                                                    
+   tmp = current;                                                               
+                                                                                  
+   while(tmp->pid!=1){                                                          
+         printk("id: %d, name: %s\n", tmp->pid,tmp->comm);                          
+         tmp = tmp->real_parent;                                                  
+   }                                                                            
+                                                                                   
+   printk("id: %d, name: %s\n", tmp->pid,tmp->comm);                              
+                                                                                  
+   return(0);                                                                   
+}            
