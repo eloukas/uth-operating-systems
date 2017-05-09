4d1100c1adc0365535f71e97530d6c5246cef902
Eleftherios P. Loukas <eleftheriosloukas@gmail.comgit config --global user.email eleftheriosloukas@gmail.com>
2017-05-09 22:55:15 +0300

    blank lines in .sh script-better visually

--- hw3/syscall/make_static_library.sh
+++ hw3/syscall/make_static_library.sh
@@ -4,19 +4,23 @@ A static library is basically a set of object files that were copied into a sing
 echo "1--> .c file for the library"
 echo "2--> create object .o"
 echo "3--> with .o create a static library"
+echo ""
 echo "We always forget filenames my friend.There you go!"
 ls *.c
 echo "Type .c (**without .c suffix!) file, followed by [ENTER]:"
+echo ""
 read source_code
 gcc -Wall -c "$source_code.c" -o "$source_code.o"
 echo "Created $source_code.o"
 echo "Time for static library :)"
-echo "Type static library's name, followed by [ENTER]:"
+echo "Type static library's name(without suffix), followed by [ENTER]:"
+echo ""
 read library_name
 ar rcs "lib$library_name.a" "$source_code.o"
 echo "Done with library"
 echo "Linking against static library"
-echo "Type main program .c, followed by [ENTER]:"
+echo "Type your main program's .c name (without suffix), followed by [ENTER]:"
+echo ""
 read main
 gcc -Wall -static "$main.c" -L. -l"$library_name" -o $main
 
