ddf388e42b32f5e5b0cfc5a531166bc400491801
Klainti Bontourri <kbontourri@gmail.com>
2017-05-11 01:25:44 -0700

    syscall call properly

--- hw3/syscall/check_space_and_get_measurements.c
+++ hw3/syscall/check_space_and_get_measurements.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include "syscall_wrapper.h"
 
-
 int main(int argc, char* argv[] ) {
 
 	//Print Allocated Memory
-	printf("Average memory which is allocated: %lu \n",slob_get_total_alloc_mem());
+	printf("Average memory which is allocated: %lu \n",get_total_alloc_mem_syscall_wrapper());
 
 	//Print Free Memory
-	printf("Average memory which is free: %lu \n", slob_get_total_free_mem());
+	printf("Average memory which is free: %lu \n", get_total_free_mem_syscall_wrapper());
 
 	return 0;
 }
